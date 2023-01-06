e62b17b22bece91a649eb3fc8b179de90b5dfbf0
Stanislas Marçais / Knupel <stanislasiii@gmail.com>
2023-01-06 17:37:20 +0100

    clarify readme and code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "./pages/layout";
 import { Resto }  from "./pages/resto";
--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,27 +1,6 @@
 import {Outlet, Link} from "react-router-dom";
 
-// function Menu() {
-// 	return (
-// 		<div>
-// 			<ul>
-// 				<li>
-// 					<Link to="/">Resto</Link>
-// 				</li>
-// 				<li>
-// 					<Link to="/entrees">Entrées</Link>
-// 				</li>
-// 				<li>
-// 					<Link to="/plats">Plats</Link>
-// 				</li>
-// 				<li>
-// 					<Link to="/desserts">Desserts</Link>
-// 				</li>
-// 			</ul>
-// 		</div>
-// 	)
-// }
-
-const Menu = () => {
+function Menu() {
 	return (
 		<div>
 			<ul>
